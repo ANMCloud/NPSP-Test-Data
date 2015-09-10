2b1e3be40b615b6ee5a4af7ef17a8312f6b59397
Jason Lantz <jlantz@salesforce.com>
2015-09-09 22:56:20 -0500

    Fix make_unmanaged.sh to modify the directory for transforms in the created jobs

--- make_unmanaged.sh
+++ make_unmanaged.sh
@@ -9,11 +9,23 @@ for file in ../../'Test Data Deployment'/Delete/*.k*; do
     echo "Writing Delete/$filename"
     sed -e "s/npsp__//g" "$file" > "$filename"
 done
+for file in *.kjb; do
+    filename=`basename "$file"`.new
+    echo "Writing Delete/$filename"
+    sed -e "s/x2f;Test Data Deployment/x2f;Test Data Deployment Unmanaged/g" "$file" > "$filename"
+    mv "$filename" "$file"
+done
 cd ..
 
 for file in ../'Test Data Deployment'/*.k*; do
     filename=`basename "$file"`
     echo "Writing $filename"
     sed -e "s/npsp__//g" "$file" > "$filename"
+done
+for file in *.kjb; do
+    filename=`basename "$file"`.new
+    echo "Writing Delete/$filename"
+    sed -e "s/x2f;Test Data Deployment/x2f;Test Data Deployment Unmanaged/g" "$file" > "$filename"
+    mv "$filename" "$file"
 done
 cd ..
